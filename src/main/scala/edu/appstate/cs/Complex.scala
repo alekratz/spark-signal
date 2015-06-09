e7c73587217ca0f9178c4a9ab17f029d978b02b1
Alek Ratzloff <alekratz@gmail.com>
2015-06-09 13:25:59 -0400

    Add actual FFT/Complex number code

--- src/main/scala/edu/appstate/cs/Complex.scala
+++ src/main/scala/edu/appstate/cs/Complex.scala
@@ -1,8 +1,32 @@
 package edu.appstate.cs
 
-/**
- * Created by alek on 6/4/15.
- */
-class Complex {
+case class Complex(re: Double, im: Double) {
+  def +(x: Complex): Complex = Complex(re + x.re, im + x.im)
+  def -(x: Complex): Complex = Complex(re - x.re, im - x.im)
+  def *(x: Double): Complex = Complex(re * x, im * x)
+  def *(x: Complex): Complex = Complex(re * x.re - im * x.im, re * x.im + im * x.re)
+  def /(x: Double):  Complex = Complex(re / x, im / x)
 
+  override def toString: String = {
+    val a = "%1.3f" format re
+    val b = "%1.3f" format im
+    (a, b) match {
+      case (_, "0.000") => a
+      case ("0.000", _) => b + "i"
+      case (_, _) if im > 0 => a + " + " + b + "i"
+      case (_, _) => a + " - " + b + "i"
+    }
+  }
+}
+
+object Complex {
+  def parse(str: String): Complex = {
+    var re: Double = 0
+    var im: Double = 0
+    str.replace(" ", "")
+      .split('+')
+      .foreach(p => if(p.endsWith("i")) im += p.replace("i", "").toDouble
+                    else re += p.toDouble)
+    Complex(re, im)
+  }
 }
--- src/main/scala/edu/appstate/cs/FFT.scala
+++ src/main/scala/edu/appstate/cs/FFT.scala
@@ -1,8 +1,75 @@
 package edu.appstate.cs
 
-/**
- * Created by alek on 6/4/15.
- */
-object FFT {
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+import scala.math.{Pi, cosh, sinh, cos, sin}
 
+object FFT {
+  def fft(cSeq: RDD[Complex], direction: Complex, scalar: Int): RDD[Complex] = {
+    val n = cSeq.count().toInt
+    if(n == 1) {
+      return cSeq
+    }
+    assume(n % 2 == 0, "Cooley-Tukey FFT algorithm expects an even-length input of values")
+
+    val (evenRef, oddRef) = getEvenOddPairs(cSeq)
+    val evens = fft(evenRef, direction, scalar).collect()
+    val odds = fft(oddRef, direction, scalar).collect()
+
+    def leftRightPair(k: Int): (Complex, Complex) = {
+      val base = evens(k) / scalar
+      val offset = exp(direction * (Pi * k / n)) * odds(k) / scalar
+      (base + offset, base - offset)
+    }
+
+    val pairs = (0 until n / 2) map leftRightPair
+    val left = pairs.map(_._1)
+    val right = pairs.map(_._2)
+    cSeq.context.parallelize(left ++ right)
+  }
+
+  def  fft(cSeq: RDD[Complex]): RDD[Complex] = fft(cSeq, Complex(0, 2), 1)
+  def ifft(cSeq: RDD[Complex]): RDD[Complex] = fft(cSeq, Complex(0, -2), 2)
+
+  def main(args: Array[String]): Unit = {
+    if(args.length == 0 || args.length % 2 != 0) {
+      println("usage: FFT item1 item2 [ item3 item4 ... ]")
+      println("FFT expects an even number of items in the sequence passed to it")
+      System.exit(1)
+    }
+    val appName = "ScalaSignal"
+    val conf = new SparkConf().setAppName(appName)
+    val sc = new SparkContext(conf)
+
+    // construct a sequence
+    val complexes = sc.parallelize(
+      args.map(p => Complex.parse(p)))
+    println("read complex values")
+    complexes.collect()
+      .foreach(cpx => println(cpx))
+    val fourier = fft(complexes)
+    val inverse = ifft(fourier)
+
+    println("results of FFT")
+    fourier.collect()
+      .foreach(cpx => println(cpx))
+    println("results of IFFT")
+    inverse.collect()
+      .foreach(cpx => println(cpx))
+  }
+
+  private def exp(c: Complex): Complex = {
+    val r = cosh(c.re) + sinh(c.re)
+    Complex(cos(c.im), sin(c.im)) * r
+  }
+
+  private def getEvenOddPairs(toGroup: RDD[Complex]): (RDD[Complex], RDD[Complex]) = {
+    val evens = toGroup.zipWithIndex()
+      .filter(tup => tup._2 % 2 == 0)
+      .map(tup => tup._1)
+    val odds = toGroup.zipWithIndex()
+      .filter(tup => tup._2 % 2 == 1)
+      .map(tup => tup._1)
+    (evens, odds)
+  }
 }
